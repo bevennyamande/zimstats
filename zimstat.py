915000d979ad1682a7eadd9e8d880c8f3c8a42a5
bevennyamande <bevennyamande@gmail.com>
2019-07-23 00:53:04 +0200

    zimstats

--- zimstat.py
+++ zimstat.py
@@ -1,9 +1,10 @@
 #!usr/bin/env python
 
 import pandas as pd
+
 # read the csv file into a dataframe
-df = pd.read_csv('zimdat_online_2017_en.csv', encoding='utf-8')
-df = df.groupby(by='Indicator')
-indicators = df['Indicator']
+df = pd.read_csv("zimdat_online_2017_en.csv", encoding="utf-8")
+df = df.groupby(by="Indicator")
+indicators = df["Indicator"]
 print(indicators)
 # print(df.describe())
