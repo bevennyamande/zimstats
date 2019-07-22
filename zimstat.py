7261f4dbba2c33ea9cd9c3bb6279fe6388dbb955
bevennyamande <bevennyamande@gmail.com>
2019-07-23 01:38:36 +0200

    zimstats

--- zimstat.py
+++ zimstat.py
@@ -4,7 +4,12 @@ import pandas as pd
 
 # read the csv file into a dataframe
 df = pd.read_csv("zimdat_online_2017_en.csv", encoding="utf-8")
-df = df.groupby(by="Indicator")
-indicators = df["Indicator"]
-print(indicators)
-# print(df.describe())
+
+# drop all NaN values
+df = df.dropna(subset=['Data Value'], inplace=True)
+
+print(df)
+#df = df.groupby(by="Indicator")
+#indicators = df["Indicator"]
+#print(indicators)
+#print(df.describe())
